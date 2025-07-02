34c19c0d6f06437f0aabe324e9e089b253437a5a
Sebastian Kliś <klis.seb@gmail.com>
2025-07-02 21:01:06 +0200

    rearranged grid layout to properly render on every device size

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Card from "./components/Card/Card.jsx";
 import Container from "./components/Container/Container.jsx";
+import UserCard from "./components/UserCard/UserCard.jsx";
 
 function App() {
 
     return (
         <>
             <Container>
+                <UserCard/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
                 <Card/>
             </Container>
         </>
---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,12 @@
+import './UserCard.scss';
+
+function UserCard() {
+    return (
+        <div className="user-card">
+            <h2>Hello</h2>
+            <p>Name Surname</p>
+        </div>
+    )
+}
+
+export default UserCard;
