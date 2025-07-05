645805e88d261b253d0752b5e83ed8ebced56c5f
Sebastian Kliś <klis.seb@gmail.com>
2025-07-05 15:54:25 +0200

    created getData() to fetch values from data.json file and display them dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 import Card from "./components/Card/Card.jsx";
 import Container from "./components/Container/Container.jsx";
 import UserCard from "./components/UserCard/UserCard.jsx";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [data, setData] = useState([]);
+    const getData = () => {
+        fetch('data.json'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson);
+            });
+    }
+    useEffect(() => {
+        getData()
+    }, []);
 
     return (
         <>
             <Container>
                 <UserCard/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {
+                    data && data.length > 0 && data.map((item) =>
+                        <Card key={item.title} title={item.title}
+                              currentTime={item.timeframes.daily.current}
+                              previousTime={item.timeframes.daily.previous}/>)
+                }
+
             </Container>
         </>
     )
 }
 
 export default App
+
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,17 @@
 import './Card.scss'
 import iconEllipsis from '../../assets/images/icon-ellipsis.svg';
 
-function Card() {
+function Card({title, currentTime, previousTime}) {
     return (
         <div className="card">
             <div className="card__title-group row">
-                <h2 className="card__name">Work</h2>
+                <h2 className="card__name">{title}</h2>
                 <img src={iconEllipsis} alt="" className="card__dots"/>
             </div>
-            <h3 className="card__time">32hrs</h3>
-            <h3 className="card__last-week">Last Week - 36hrs</h3>
+            {currentTime === 1 ? <h3 className="card__time">{currentTime} hr</h3> :
+                <h3 className="card__time">{currentTime} hrs</h3>}
+
+            <h3 className="card__last-week">{previousTime} hrs</h3>
         </div>
     )
 }
